16505d31bf0f75586003910133878b4319871ef0
RoomyTortoise <4ceved0.j0rge@gmail.com>
2023-03-30 15:51:15 -0600

    changes

---
+++ functions/submit_contact_form.js
@@ -0,0 +1,28 @@
+const faunadb = require('faunadb');
+const q = faunadb.query;
+
+const client = new faunadb.Client({
+  secret: process.env.fnAFAU8J5QAAzkgZkkVBkt0pj6CekDTDS4yLt-v9
+
+});
+
+exports.handler = async function (event, context) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const { name, email, message } = JSON.parse(event.body);
+
+  try {
+    await client.query(
+      q.Create(q.Collection('contact_form_entries'), {
+        data: { name, email, message }
+      })
+    );
+
+    return { statusCode: 200, body: 'Message saved successfully' };
+  } catch (error) {
+    console.error('Error saving message:', error);
+    return { statusCode: 500, body: 'Error saving message' };
+  }
+};
